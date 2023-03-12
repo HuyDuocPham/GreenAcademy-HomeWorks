9c66bc8d8954fa48bf3bc4a4ae5e81228601b827
DESKTOP-5C9VHO1\PHD <huyduocphamm@gmail.com>
2023-03-13 02:47:40 +0700

    hello

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import Bai01 from './components/Bai01'
+
+const App = () => {
+  return (
+    <Bai01/>
+  );
+}
+
+export default App;
+/**
+ * BÀI TẬP REACT
+
+Bài 1:
+Tạo một useState products (name, code, price)
+Hiển thị danh sách products ra màn hình
+
+Bài 2:
+Tạo một component có:
+input cho email
+input cho password
+button login
+Sau khi nhập email và password.
+Click button login sẽ hiện lên thông báo: “Đăng nhập thành công".
+
+Bài 3:
+Tạo một input để nhập vào một dãy số
+Chứ mỗi khi nhập một con số bất kì
+Màn hình sẽ hiện ra tổng của từng ký tự của số đó.
+
+Bài 4:
+Tạo 2 input firstName và lastName
+Khi nhập vào những ký tự bất kì
+Hiển thị fullName lên màn hình.
+
+Bài 5:
+Tạo 2 components
+Component đầu tiên hiển thị thông tin user (name, email) bất kì.
+Component thứ 2 được gọi từ component thứ 1, hiển thị những products (name, price) mà người dùng này đã mua.
+
+Bài 6:
+Tạo 1 component
+Có 1 input để nhập số n, 1 input để nhập 1 đoạn text t.
+Thêm 1 component để hiển thị một danh sách
+danh sách này hiển thị n lần text t.
+
+ */
