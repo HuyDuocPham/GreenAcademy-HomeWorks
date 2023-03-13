55803b715b1a12e2f451f9334ec586587dce7f4a
DESKTOP-5C9VHO1\PHD <huyduocphamm@gmail.com>
2023-03-13 17:57:01 +0700

    Bai01

--- src/components/Bai01/index.js
+++ src/components/Bai01/index.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
 
 const App = () => {
-    
-    return (
-        
+    const [list, setList] = useState({name: 'aaa', code: '111', price: 10})
+//   const [lists, setLists] = useState([
+//     { name: "A", code: "1", price: 10 },
+//     { name: "B", code: "2", price: 20 },
+//     { name: "C", code: "3", price: 30 },
+//     { name: "D", code: "4", price: 40 },
+//   ]);
+  return (
         <div>
-            Hello
+          <div>Tên sản phẩm: {list.name}</div>
+          <div>Mã sản phẩm: {list.code}</div>
+          <div>Giá sản phẩm: {list.price}</div>
         </div>
-        
-    )
-}
+  );
+};
 export default App;
