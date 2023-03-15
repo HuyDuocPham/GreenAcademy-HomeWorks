ed03ec7a7203d0f592c74892005a58ecc4e06a7a
DESKTOP-5C9VHO1\PHD <huyduocphamm@gmail.com>
2023-03-15 16:51:14 +0700

    Baii01

--- src/components/Bai01/index.js
+++ src/components/Bai01/index.js
@@ -1,19 +1,24 @@
 import { useState } from "react";
 
 const App = () => {
-    const [list, setList] = useState({name: 'aaa', code: '111', price: 10})
-//   const [lists, setLists] = useState([
-//     { name: "A", code: "1", price: 10 },
-//     { name: "B", code: "2", price: 20 },
-//     { name: "C", code: "3", price: 30 },
-//     { name: "D", code: "4", price: 40 },
-//   ]);
+  const [lists] = useState([
+    { name: "A", code: "1", price: 10 },
+    { name: "B", code: "2", price: 20 },
+    { name: "C", code: "3", price: 30 },
+    { name: "D", code: "4", price: 40 },
+  ]);
   return (
-        <div>
-          <div>Tên sản phẩm: {list.name}</div>
-          <div>Mã sản phẩm: {list.code}</div>
-          <div>Giá sản phẩm: {list.price}</div>
-        </div>
+    <div>
+      {lists.map((item) => {
+        return (
+          <tr>
+            <td>Tên sản phẩm: {item.name}</td>
+            <td>Mã sản phẩm: {item.code}</td>
+            <td>Giá sản phẩm: {item.price}</td>
+          </tr>
+        );
+      })}
+    </div>
   );
 };
 export default App;
