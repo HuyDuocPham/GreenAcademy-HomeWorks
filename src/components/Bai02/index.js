dff15a951059329c97f844ada66ab406532572a3
DESKTOP-5C9VHO1\PHD <huyduocphamm@gmail.com>
2023-03-17 16:59:16 +0700

    Bai02

--- src/components/Bai02/index.js
+++ src/components/Bai02/index.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
+
 const App = () => {
+  const [user, setUser] = useState({name: "", email: ""})
+
+  const onChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    })
+  }
 
   const onClick = () => {
     alert("Đăng nhập thành công");
-  };
+  }
+
   return (
-    <>
-      <input name="name" placeholder="Name." />
-      <input name="email" placeholder="Email." />
+    <div>
+      <input name="name" value={user.name} placeholder="Name." onChange={onChange} />
+      <input name="email" value={user.email} placeholder="Email." onchange={onChange} />
       <button onClick={onClick}>Log In</button>
-    </>
+    </div>
   );
 };
 
